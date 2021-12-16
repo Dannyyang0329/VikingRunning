d882601bba3862025c83b651ed564c9259085316
Dannyyang0329 <f74094067@gs.ncku.edu.tw>
2021-12-16 19:10:31 +0800

    Camera adjust create building
    Camera follows player more smooth.
    Import packages and create buildings.

--- Assets/Scripts/Game_Scripts/PlayerController.cs
+++ Assets/Scripts/Game_Scripts/PlayerController.cs
@@ -4,24 +4,62 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float movingSpeed;
+    public float movingSpeed = 15f;
 
+    public float duration = 0.2f;
 
+    public float jumpForce = 100f;
+    private bool isGrounded = true;
+
+    private bool isTuring = false;
+    private Quaternion start;
+    private Quaternion end;
+    private float rate;
+    private float time = 0;
+
+    private void Start() {
+        rate = 1 / duration;
+    }
     private void Update() {
-        if(Input.GetKeyDown(KeyCode.A)) {
-            Vector3 degree = new Vector3(0, 90, 0);
-            Vector3 newRot = transform.rotation.eulerAngles - degree;
+        if (isTuring) {
+            time += rate * Time.deltaTime;
+            transform.rotation = Quaternion.Slerp(start, end, time);
 
-            transform.rotation = Quaternion.Euler(newRot);
+            if(time > 1.0f) {
+                transform.rotation = Quaternion.Slerp(start, end, 1);
+                isTuring = false;
+            }
+        }
+        else time = 0;
+
+        transform.Translate(Vector3.forward * movingSpeed * Time.deltaTime);            
+
+        if(Input.GetKeyDown(KeyCode.A)) {
+            SetRotateStatus(true);
+            isTuring = true;
         }
         else if(Input.GetKeyDown(KeyCode.D)) {
-            Vector3 degree = new Vector3(0, 90, 0);
-            Vector3 newRot = transform.rotation.eulerAngles + degree;
+            SetRotateStatus(false);
+            isTuring = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.W) && isGrounded) {
+            gameObject.GetComponent<Rigidbody>().AddForce(jumpForce * Vector3.up);
+            isGrounded = false;
+        }
+    }
 
-            transform.rotation = Quaternion.Euler(newRot);
+    private void OnCollisionEnter(Collision collision) {
+        if(collision.collider.CompareTag("Ground")) {
+            isGrounded = true;
         }
-        else {
-            transform.Translate(Vector3.forward * movingSpeed * Time.deltaTime);            
-        }
+    }
+
+    void SetRotateStatus(bool isTuringLeft) {
+        Vector3 degree;
+        if (isTuringLeft) degree = new Vector3(0, -90,  0);
+        else degree = new Vector3(0, 90,  0);
+
+        start = transform.rotation;
+        end = start * Quaternion.Euler(degree);
     }
 }
